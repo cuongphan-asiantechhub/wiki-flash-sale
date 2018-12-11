5ca95503a41f2c0e86fbd1530033ebfe44e38f5c
phnguyentri <phnguyentri@gmail.com>
2018-12-11 10:42:41 +0700

    first commit

---
+++ Marvelic/FlashSale/Plugin/FlashSale.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace Marvelic\FlashSale\Plugin;
+
+class FlashSale
+{
+
+    public function afterDoAction(\FlashSale $subject, $result)
+    {
+        //Your plugin code
+    }
+}
